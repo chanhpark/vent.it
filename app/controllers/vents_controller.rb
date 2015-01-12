2b98c2807ad46cbf736fd438cc3e9133d79329d9
chanhpark <chanhyun.park1211@gmail.com>
2015-01-11 22:36:18 -0500

    took out destroy method in vents controller(unnesssary code)

--- app/controllers/vents_controller.rb
+++ app/controllers/vents_controller.rb
@@ -18,9 +18,6 @@ class VentsController < ApplicationController
     if @vent.update(vent_params)
       redirect_to admin_vents_path
       flash[:notice] = "Your Vent has been successfully updated."
-    else
-      render :edit
-      flash[:notice] = "Please fill out the forms correctly."
     end
   end
 
@@ -31,13 +28,6 @@ class VentsController < ApplicationController
       redirect_to vent_path(@vent)
     else
       render "new"
-    end
-  end
-
-  def destroy
-    @vent = Vent.find(params[:id])
-    if @vent.destroy
-      redirect_to admin_path, notice: "Vent Deleted Successfully"
     end
   end
 
