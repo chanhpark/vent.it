5a71b43f3b77c77dd391012e3fe920263a1fc4dc
chanhpark <chanhyun.park1211@gmail.com>
2015-01-23 11:42:33 -0500

    count method needed so it can parse the json

--- app/models/word.rb
+++ app/models/word.rb
@@ -2,6 +2,14 @@ class Word < ActiveRecord::Base
   has_many :word_counts
   validates :word, presence: true, uniqueness: true
 
+  def count
+    total = 0
+    word_counts.each do |value|
+      total += value.count
+    end
+    total
+  end
+
   def _id
     word
   end
