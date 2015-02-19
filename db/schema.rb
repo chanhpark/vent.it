09b8eb70ff78808dda195bb833ab0090bd08a38f
chanhpark <chanhyun.park1211@gmail.com>
2015-02-19 16:48:34 -0500

    index to words

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219213557) do
+ActiveRecord::Schema.define(version: 20150219214719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,5 +89,7 @@ ActiveRecord::Schema.define(version: 20150219213557) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "words", ["word"], name: "index_words_on_word", using: :btree
 
 end
